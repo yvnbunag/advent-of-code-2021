16ff56bfe6dad28c08bd8f21587fd9ecc413b76a
Ian Buñag <jianbunag@gmail.com>
2021-12-04 12:47:24 +0800

    Scaffold puzzle template

---
+++ test/specs/puzzle/template.ts
@@ -0,0 +1,22 @@
+import {} from '~/puzzle/day-1'
+import { getInput } from '~/inputs'
+
+describe('Part 1', () => {
+  it('should answer example', () => {
+    const data = ``
+  })
+
+  it('should answer puzzle', () => {
+    const data = getInput('day-1')
+  })
+})
+
+describe('Part 2', () => {
+  it('should answer example', () => {
+    const data = ``
+  })
+
+  it('should answer puzzle', () => {
+    const data = getInput('day-1')
+  })
+})
