4df8248e97a38e4089bb472cc7d6caecb1a53fc9
Ian Buñag <jianbunag@gmail.com>
2021-12-04 12:53:14 +0800

    Scaffold day 4 puzzle

---
+++ src/puzzle/day-4.ts
@@ -0,0 +1,13 @@
+import { parseInputToList } from '~/puzzle/utils'
+
+type First = void
+
+type Second = void
+
+export function first(input: string): First {
+  const data = parseInputToList(input)
+}
+
+export function second(input: string): Second {
+  const data = parseInputToList(input)
+}
---
+++ test/specs/puzzle/day-4.ts
@@ -0,0 +1,22 @@
+import {} from '~/puzzle/day-4'
+import { getInput } from '~/inputs'
+
+describe('Part 1', () => {
+  it('should answer example', () => {
+    const data = ``
+  })
+
+  it('should answer puzzle', () => {
+    const data = getInput('day-4')
+  })
+})
+
+describe('Part 2', () => {
+  it('should answer example', () => {
+    const data = ``
+  })
+
+  it('should answer puzzle', () => {
+    const data = getInput('day-4')
+  })
+})
