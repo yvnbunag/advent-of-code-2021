9d9dc4c2a858328e53453dc87216f0a8ed22377d
Ian Buñag <jianbunag@gmail.com>
2021-12-05 12:53:05 +0800

    Add logging to scaffold script

--- scaffold.sh
+++ scaffold.sh
@@ -4,12 +4,24 @@ inputsDirectory='src/inputs'
 puzzleDirectory='src/puzzle'
 specsDirectory='test/specs/puzzle'
 
+function scaffolded {
+  echo "scaffolded $1"
+}
+
 if [ -z "$day" ]
   then
     echo "Day (number) argument required"
     exit 1
 fi
 
-cp -n "$inputsDirectory/template.txt" "$inputsDirectory/day-$day.txt"
-cp -n "$puzzleDirectory/template.ts" "$puzzleDirectory/day-$day.ts"
-cp -n "$specsDirectory/template.spec.ts" "$specsDirectory/day-$day.spec.ts"
+targetInput="$inputsDirectory/day-$day.txt"
+targetPuzzle="$puzzleDirectory/day-$day.ts"
+targetSpec="$specsDirectory/day-$day.spec.ts"
+
+# @TODO prevent if files already exist
+cp -n "$inputsDirectory/template.txt" $targetInput
+scaffolded $targetInput
+cp -n "$puzzleDirectory/template.ts" $targetPuzzle
+scaffolded $targetPuzzle
+cp -n "$specsDirectory/template.spec.ts" $targetSpec
+scaffolded $targetSpec
